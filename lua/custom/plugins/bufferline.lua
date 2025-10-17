cc73c531b0d5c77bc51a64561e5559d1e01f9041
Chris Gray <chgray@gmail.com>
2025-10-16 20:15:21 -0700

    working

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,11 +3,80 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    options = {
-      mode = 'buffers',
-      themable = true,
-      close_icon = 'x',
-      path_components = 1,
+    require('bufferline').setup {
+      options = {
+        mode = 'buffers', -- set to "tabs" to only show tabpages instead
+        themable = true,
+        numbers = 'ordinal', -- show buffer numbers (can be "none", "ordinal", "buffer_id", or "both")
+        close_command = 'bdelete! %d', -- can be a string | function, see "Mouse actions"
+        right_mouse_command = 'bdelete! %d', -- can be a string | function, see "Mouse actions"
+        left_mouse_command = 'buffer %d', -- can be a string | function, see "Mouse actions"
+        middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+        indicator = {
+          icon = '▎', -- this should be omitted if indicator style is not 'icon'
+          style = 'icon', -- can be 'icon' or 'underline'|'none'
+        },
+
+        buffer_close_icon = '',
+        modified_icon = '●',
+        close_icon = '',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+        max_name_length = 18,
+        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        truncate_names = true, -- whether or not tab names should be truncated
+        tab_size = 18,
+        diagnostics = 'nvim_lsp', -- show LSP diagnostics in the bufferline
+        diagnostics_update_in_insert = false,
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match 'error' and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
+        -- NOTE: this will be called a lot so don't do any heavy processing here
+        custom_filter = function(buf_number, buf_numbers)
+          -- filter out filetypes you don't want to see
+          if vim.bo[buf_number].filetype ~= 'qf' then
+            return true
+          end
+        end,
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'File Explorer',
+            text_align = 'left',
+            separator = true,
+          },
+        },
+        color_icons = true, -- whether or not to add the filetype icon highlights
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+        show_tab_indicators = true,
+        show_duplicate_prefix = true,
+        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+        separator_style = 'thin', -- can be "slant" | "slope" | "thick" | "thin" | { 'any', 'any' }
+        enforce_regular_tabs = false,
+        always_show_bufferline = true,
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { 'close' },
+        },
+        sort_by = 'insert_after_current', -- can be 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+      },
     }
+
+    -- Keymaps for buffer navigation
+    vim.keymap.set('n', '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', { desc = 'Toggle [B]uffer [P]in' })
+    vim.keymap.set('n', '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', { desc = '[B]uffer Delete Non-[P]inned' })
+    vim.keymap.set('n', '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', { desc = '[B]uffer Delete [O]thers' })
+    vim.keymap.set('n', '<leader>br', '<Cmd>BufferLineCloseRight<CR>', { desc = '[B]uffer Delete to [R]ight' })
+    vim.keymap.set('n', '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', { desc = '[B]uffer Delete to [L]eft' })
+    vim.keymap.set('n', '<S-h>', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Prev Buffer' })
+    vim.keymap.set('n', '<S-l>', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Next Buffer' })
+    vim.keymap.set('n', '[b', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Prev Buffer' })
+    vim.keymap.set('n', ']b', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Next Buffer' })
+    vim.keymap.set('n', '[B', '<Cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer prev' })
+    vim.keymap.set('n', ']B', '<Cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer next' })
   end,
 }
